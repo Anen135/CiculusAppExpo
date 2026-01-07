8afaf8ef3e6c87b3ae148544811c54d5a5bfaa49
Anen135 <whoyou1994j4920@gmail.com>
2026-01-07 17:23:14 +0500

    Revert "Merge pull request #2 from Anen135/feat/db-access-optimization-3197600086593797153"
    This reverts commit c712b5e6f75557fb448681ba9b25be150c144359, reversing
    changes made to c1814f4547e491691c62cc5d6e72ee09427aa93a.

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,6 +1,6 @@
 import { DayDiary } from "@/components/DayDiary";
 import { DayTimeline } from "@/components/DayTimeline";
-import { useSettings } from "@/context/SettingsContext";
+import { useSettings } from "@/context/SettingsСontext";
 import { useTheme } from "@/context/ThemeContext";
 import { DiaryEntry, useDiary } from "@/hooks/useDiary";
 import { getLocalDateStr } from "@/utils/dateUtil";
@@ -18,14 +18,8 @@ export default function MainPage() {
   const { colors } = useTheme();
   const { dayTimelineViewMode  } = useSettings();
 
-  const loadCurrentDayEntries = useCallback(() => {
-    loadEntries(getLocalDateStr(currentDay));
-  }, [currentDay, loadEntries]);
-
-  useFocusEffect(loadCurrentDayEntries);
-  useEffect(() => {
-    loadCurrentDayEntries();
-  }, [loadCurrentDayEntries]);
+  useFocusEffect(useCallback(() => { loadEntries(); }, [loadEntries]));
+  useEffect(() => { loadEntries(); }, [currentDay, loadEntries]);
 
   const headerText = (() => {
     const now = new Date();
@@ -49,6 +43,9 @@ export default function MainPage() {
   })();
 
 
+  const dayEntries = entries.filter(e => e.Date === getLocalDateStr(currentDay));
+
+
   const renderItem = ({ item }: { item: DiaryEntry }) => (
     <TouchableOpacity
       onPress={() => router.push({ pathname: "/entry", params: { entry: JSON.stringify(item) } })}
@@ -69,12 +66,12 @@ export default function MainPage() {
         </Text>
         {
           dayTimelineViewMode === "v1" ?
-          <DayTimeline entries={entries} /> :
-          <DayDiary entries={entries} />
+          <DayTimeline entries={dayEntries} /> :
+          <DayDiary entries={dayEntries} />
         }
 
         <FlatList
-          data={entries}
+          data={dayEntries}
           keyExtractor={(item) => item.Id.toString()}
           ListEmptyComponent={ <Text style={{ textAlign: "center", fontSize: 16, marginVertical: 20, color: "#666" }}> {i18n.t('main.noEntries')} </Text> }
           renderItem={renderItem}
--- hooks/useDiary.ts
+++ hooks/useDiary.ts
@@ -17,29 +17,23 @@ export interface DiaryEntry {
 export function useDiary() {
   const db = useSQLiteContext();
   const [entries, setEntries] = useState<DiaryEntry[]>([]);
-  const loadEntries = useCallback(
-    async (date?: string) => {
-      try {
-        let query = `
+  const loadEntries = useCallback(async () => {
+    try {
+      const all = await db.getAllAsync<DiaryEntry>(`
         SELECT
           Id, Date, StartTime, EndTime, Name, Notes, Color, Tags,
           (strftime('%s', '2000-01-01 ' || EndTime) - strftime('%s', '2000-01-01 ' || StartTime)) AS DurationSeconds
         FROM DiaryEntry
-      `;
-        const params: any[] = [];
-        if (date) {
-          query += " WHERE Date = ?";
-          params.push(date);
-        }
-        query += " ORDER BY Date DESC, StartTime ASC;";
-        const result = await db.getAllAsync<DiaryEntry>(query, params);
-        setEntries(result);
-      } catch (e) {
-        console.error("Ошибка при загрузке записей:", e);
-      }
-    },
-    [db]
-  );
+        ORDER BY Date DESC, StartTime ASC;
+      `);
+      setEntries(all);
+    } catch (e) {
+      console.error("Ошибка при загрузке записей:", e);
+    }
+  }, [db]);
+  useEffect(() => {
+      loadEntries();
+  }, [loadEntries]);
 
 
   const addEntry = async ( entry: Partial<Omit<DiaryEntry, "Id" | "DurationSeconds">> ) => {
